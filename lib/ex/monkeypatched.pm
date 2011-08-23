ea53dae895dae9aa37cb932580e389b2deda0db6
Aaron Crane <arc@aaroncrane.co.uk>
2011-08-23 16:06:09 +0100

    Minimal dist and module boilerplate

---
+++ lib/ex/monkeypatched.pm
@@ -0,0 +1,25 @@
+package ex::monkeypatched;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ex::monkeypatched - Experimental API for safe monkey-patching
+
+=head1 AUTHOR
+
+Aaron Crane E<lt>arc@cpan.orgE<gt>
+
+=head1 LICENCE
+
+This library is free software; you can redistribute it and/or modify it
+under the terms of either the GNU General Public License version 2 or, at
+your option, the Artistic License.
+
+=cut
